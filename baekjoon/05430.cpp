4139599657250f79455e2613c9c83257635e14f8
khhandrea <khhandrea@naver.com>
2025-11-26 11:49:42 +0900

    Remove cpp-practice

---
+++ baekjoon/05430.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    short T;
+
+    std::cin >> T;
+
+    for (short t = 0; t < T; t++) {
+        std::cout << "Hello world\n";
+    }
+
+    return 0;
+}
---
+++ baekjoon/17626.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int n, cur;
+    int *counts;
+
+    std::cin >> n;
+    counts = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        
+    }
+
+    return 0;
+}
